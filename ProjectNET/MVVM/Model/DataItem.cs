368820eac0137727c8d1ed6b6afb8a03d06a06e9
olinzer <intangible.dream.dev@gmail.com>
2016-11-27 00:53:46 -0600

    Proyecto en blanco

---
+++ ProjectNET/MVVM/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MVVM.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
